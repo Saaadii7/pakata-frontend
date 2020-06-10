b0a2f3455ff1e4c56ccda4bcdf8fb311935714a1
tayyabarif41 <tayyab_arif41@yahoo.com>
2020-06-10 19:27:41 +0500

    Basic project setup

---
+++ src/compenent/Header/header/header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class header extends Component {
+  // constructor(props){
+    // super(props);
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div>
+        <header>
+          helo
+        </header>
+      </div>
+    );
+  }
+}
+
+export default header;
